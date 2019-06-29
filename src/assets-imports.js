599f7b8b5eebc6c91ef3727bbfebd4959b4ecbdb
oliverclark15 <oliver.clark2@mail.mcgill.ca>
2019-06-28 20:53:36 -0400

    yo

--- src/assets-imports.js
+++
@@ -1,4 +0,0 @@
-require('./scss/index.scss')
-require('./assets/icons.svg')
-require('./assets/avatar.svg')
-require('./assets/no-work-icon.jpg')
--- src/helpers/linebreak-to-br.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function (text) {
-  return text
-  .split('\n')
-  .map((line, key) => <span key={key}>{line}<br /></span>)
-}
